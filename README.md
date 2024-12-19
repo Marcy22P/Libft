9af2de37d8b979a58606f98db65f33b6c0467ee2
Import-CelloMar <marcellopisani22@gmail.com>
2024-12-19 01:36:29 +0100

    All bonus functions

--- README.md
+++ README.md
@@ -1,17 +1,10 @@
 # Libft
  
-My Libft - 42 Roma Luiss
+Libft - 42 Roma Luiss
 
-Scaricare la repository
+Norminette:
+https://github.com/42School/norminette
 
-Per prima cosa, assicurati di avere Git installato sul tuo computer. Apri il terminale e naviga nella directory in cui desideri clonare il progetto. Digita:
-git clone https://github.com/Marcy22P/Libft.git
-Dopo aver clonato il repository, entra nella cartella con:
-cd Libft
-All’interno troverai il codice su cui lavorare e i file da modificare o consultare.
-
-Norminette
-git clone https://github.com/42School/norminette
-
-Tester
+Tester:
 https://github.com/0x050f/libft-war-machine
+https://github.com/FranFrau/Supreme-Tester-Libft
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mpisani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:43:35 by mpisani           #+#    #+#             */
-/*   Updated: 2024/12/17 19:54:43 by mpisani          ###   ########.fr       */
+/*   Updated: 2024/12/19 01:31:13 by mpisani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,11 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstadd_back(t_list **lst, t_list *new);
 t_list				*ft_lstlast(t_list *lst);
 int					ft_lstsize(t_list *lst);
