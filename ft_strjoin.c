b522521a50e7f72913fd930bfd18d6fab16907ec
Marcy22P <marcellopisani22@gmail.com>
2024-12-20 17:19:05 +0100

    Last push corrected

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mpisani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:42:01 by mpisani           #+#    #+#             */
-/*   Updated: 2024/12/16 16:36:32 by mpisani          ###   ########.fr       */
+/*   Updated: 2024/12/20 16:15:42 by mpisani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*conc;
 
 	j = 0;
+	i = 0;
 	i = ft_strlen(s1);
 	j = ft_strlen(s2);
 	conc = malloc((i + j + 1) * sizeof(char));
 	if (!conc)
 		return (NULL);
-	i = 0;
 	while (s1[i] != '\0')
 	{
 		conc[i] = s1[i];
 		i++;
 	}
-	j = 0;
 	while (s2[j] != '\0')
 	{
 		conc[i + j] = s2[j];
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mpisani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:43:35 by mpisani           #+#    #+#             */
-/*   Updated: 2024/12/19 01:31:13 by mpisani          ###   ########.fr       */
+/*   Updated: 2024/12/20 15:29:16 by mpisani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,7 @@ int					ft_atoi(const char *nptr);
 char				*ft_strnstr(const char *str, const char *to_find, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strchr(const char *s, int c);
 int					ft_tolower(int c);
