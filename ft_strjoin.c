feae327022b961530077b25c1f6751aece0c3e67
Marcy22P <marcellopisani22@gmail.com>
2024-12-23 14:53:40 +0100

    Fixed ft_strjoin after first moulinette correction, inizialized value after the malloc and not before

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mpisani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:42:01 by mpisani           #+#    #+#             */
-/*   Updated: 2024/12/20 16:15:42 by mpisani          ###   ########.fr       */
+/*   Updated: 2024/12/23 14:36:41 by mpisani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	j;
 	char	*conc;
 
-	j = 0;
-	i = 0;
 	i = ft_strlen(s1);
 	j = ft_strlen(s2);
 	conc = malloc((i + j + 1) * sizeof(char));
 	if (!conc)
 		return (NULL);
+	j = 0;
+	i = 0;
 	while (s1[i] != '\0')
 	{
 		conc[i] = s1[i];
